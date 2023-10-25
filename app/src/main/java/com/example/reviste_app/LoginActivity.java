fac102c40b018813a5a7cfcf3b401dbbc6fb06c7
Dani <dani@192.168.1.11>
2023-10-25 12:45:15 -0500

    Funcionalidad Login, Register y Logout con correo electronico

--- app/src/main/java/com/example/reviste_app/LoginActivity.java
+++ app/src/main/java/com/example/reviste_app/LoginActivity.java
@@ -1,14 +1,87 @@
 package com.example.reviste_app;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
+    EditText editTextEmail, editTextPassword;
+    TextView buttonLog;
+    FirebaseAuth mAuth;
+
+    ProgressBar progressBar;
+
+    TextView RegisterBut;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        // Initialize Firebase
+        mAuth = FirebaseAuth.getInstance();
+
+        editTextEmail = findViewById(R.id.email_Log);
+        editTextPassword = findViewById(R.id.password_Log);
+        buttonLog = findViewById(R.id.btn_register);
+        progressBar = findViewById(R.id.progressBarLog);
+
+        TextView textView = findViewById(R.id.registerNow); // Replace with the actual ID of your TextView
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), RegisterActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        buttonLog.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                progressBar.setVisibility(View.VISIBLE);
+                String email = editTextEmail.getText().toString();
+                String password = editTextPassword.getText().toString();
+
+                if (TextUtils.isEmpty(email)) {
+                    Toast.makeText(LoginActivity.this, "Digite su correo", Toast.LENGTH_LONG).show();
+                    return;
+                }
+                if (TextUtils.isEmpty(password)) {
+                    Toast.makeText(LoginActivity.this, "Digite su contraseña", Toast.LENGTH_LONG).show();
+                    return;
+                }
+
+                mAuth.signInWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    // Sign in success, update UI with the signed-in user's information
+                                    Toast.makeText(LoginActivity.this, "Autenticado :D.", Toast.LENGTH_SHORT).show();
+                                    Intent intent = new Intent(getApplicationContext(), LogOutActivity.class);
+                                    startActivity(intent);
+                                    finish();
+                                } else {
+                                    // If sign in fails, display a message to the user.
+                                    Toast.makeText(LoginActivity.this, "Error de autenticación.", Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+            }
+        });
     }
 }
