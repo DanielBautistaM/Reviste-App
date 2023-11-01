fd4360bcbfafb1d8b9e4c78b89a08f3c736ae783
Dani <dani@MacBook-Air-de-Dani.local>
2023-11-01 18:46:19 -0500

    lijera correccion

--- app/src/main/java/com/example/reviste_app/splashActivity.java
+++ app/src/main/java/com/example/reviste_app/splashActivity.java
@@ -1,20 +1,44 @@
-        package com.example.reviste_app;
+package com.example.reviste_app;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.animation.Animator;
+import android.content.Intent;
 import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import com.airbnb.lottie.LottieAnimationView;
 
 public class splashActivity extends AppCompatActivity {
 
-    TextView Revisteapp;
     LottieAnimationView lottie;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
-        appname = findViewById(R.id.Revisteapp);
+        lottie = findViewById(R.id.lottie);
+        lottie.addAnimatorListener(new Animator.AnimatorListener() {
+            @Override
+            public void onAnimationStart(Animator animation) {
+                // Animation started (no action needed)
+            }
+
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                // Animation has ended, navigate to the next screen or activity
+                Intent intent = new Intent(splashActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish(); // Finish the current splash activity
+            }
+
+            @Override
+            public void onAnimationCancel(Animator animation) {
+                // Animation was canceled (no action needed)
+            }
+
+            @Override
+            public void onAnimationRepeat(Animator animation) {
+                // Animation repeated (no action needed)
+            }
+        });
     }
 }
