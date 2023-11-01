248de49d5795084786b5ce7c37059bdff13948a9
Leandro <lcortes4@unab.edu.co>
2023-11-01 16:28:06 -0500

    creacion del loading page

--- app/src/main/java/com/example/reviste_app/splashActivity.java
+++ app/src/main/java/com/example/reviste_app/splashActivity.java
@@ -6,9 +6,15 @@ import android.os.Bundle;
 
 public class splashActivity extends AppCompatActivity {
 
+    TextView Revisteapp;
+    LottieAnimationView lottie;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        appname = findViewById(R.id.Revisteapp);
     }
 }
