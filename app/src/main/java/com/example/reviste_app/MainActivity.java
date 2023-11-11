d323e2107731ebc1234df8ae2f513e3783c70fdb
DanielBautistaMr <dbautista542@unab.edu.co>
2023-11-11 14:02:06 -0500

    eliminar el filtro

--- app/src/main/java/com/example/reviste_app/MainActivity.java
+++ app/src/main/java/com/example/reviste_app/MainActivity.java
@@ -52,38 +52,29 @@ public class MainActivity extends AppCompatActivity {
         btnHome.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Agrega aquí la acción que deseas para el botón de inicio (Home)
-                // Por ejemplo, puedes abrir una actividad principal o realizar alguna otra acción.
                 Intent addProductIntent = new Intent(MainActivity.this, MainActivity.class);
                 startActivity(addProductIntent);
             }
         });
 
-        // Configura OnClickListener para el botón de agregar producto (Plus)
         ImageButton btnPlus = findViewById(R.id.btnplus);
         btnPlus.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Agrega aquí la acción que deseas para el botón de agregar producto (Plus)
-                // Por ejemplo, puedes abrir AddProductActivity.
                 Intent addProductIntent = new Intent(MainActivity.this, AddProductActivity.class);
                 startActivity(addProductIntent);
             }
         });
 
-        // Configura OnClickListener para el botón de carrito
         ImageButton btnCarrito = findViewById(R.id.btncarrito);
         btnCarrito.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Agrega aquí la acción que deseas para el botón de carrito
-                // Por ejemplo, puedes abrir LogOutActivity.
                 Intent logOutIntent = new Intent(MainActivity.this, LogOutActivity.class);
                 startActivity(logOutIntent);
             }
         });
 
-        // Configura OnClickListener para el botón de filtrar
         Button btnFilter = findViewById(R.id.btnFilter);
         btnFilter.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -92,20 +83,12 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        // Handle item click events in the RecyclerView
         recyclerView.addOnItemTouchListener(new RecyclerItemClickListener(this, recyclerView, new RecyclerItemClickListener.OnItemClickListener() {
             @Override
             public void onItemClick(View view, int position) {
-                // Retrieve the clicked product
                 Product product = productList.get(position);
-
-                // Create an intent to display the product details in DetalleProductoActivity
                 Intent intent = new Intent(MainActivity.this, DetalleProductoActivity.class);
-
-                // Pass the product details as extras in the intent
                 intent.putExtra("product", product);
-
-                // Start the DetalleProductoActivity
                 startActivity(intent);
             }
 
@@ -122,13 +105,11 @@ public class MainActivity extends AppCompatActivity {
                 .addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
                     @Override
                     public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
-                        productList.clear(); // Clear previous data
+                        productList.clear();
                         for (QueryDocumentSnapshot document : queryDocumentSnapshots) {
                             Product product = document.toObject(Product.class);
                             productList.add(product);
                         }
-
-                        // Notify the adapter that the data has changed
                         adapter.notifyDataSetChanged();
                     }
                 })
@@ -136,12 +117,10 @@ public class MainActivity extends AppCompatActivity {
                     @Override
                     public void onFailure(@NonNull Exception e) {
                         Log.e("Firestore", "Error retrieving data: " + e.getMessage());
-                        // Handle errors
                     }
                 });
     }
 
-    // Método para mostrar el cuadro de diálogo de filtro
     private void showFilterDialog() {
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
         View view = getLayoutInflater().inflate(R.layout.dialog_price_filter, null);
@@ -153,21 +132,28 @@ public class MainActivity extends AppCompatActivity {
         builder.setPositiveButton("Filtrar", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
-                // Obtén los valores ingresados por el usuario
-                double minPrice = Double.parseDouble(etMinPrice.getText().toString());
-                double maxPrice = Double.parseDouble(etMaxPrice.getText().toString());
+                double minPrice = etMinPrice.getText().toString().isEmpty() ? Double.MIN_VALUE : Double.parseDouble(etMinPrice.getText().toString());
+                double maxPrice = etMaxPrice.getText().toString().isEmpty() ? Double.MAX_VALUE : Double.parseDouble(etMaxPrice.getText().toString());
 
-                // Aplica el filtro
                 retrieveDataFromFirestore(minPrice, maxPrice);
             }
         });
 
         builder.setNegativeButton("Cancelar", null);
 
+        Button btnClearFilter = view.findViewById(R.id.btnClearFilter);
+        btnClearFilter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                etMinPrice.setText("");
+                etMaxPrice.setText("");
+                retrieveDataFromFirestore();
+            }
+        });
+
         builder.show();
     }
 
-    // Overloaded method for retrieving filtered data
     private void retrieveDataFromFirestore(double minPrice, double maxPrice) {
         db.collection("Productos")
                 .whereGreaterThanOrEqualTo("price", minPrice)
@@ -176,13 +162,11 @@ public class MainActivity extends AppCompatActivity {
                 .addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
                     @Override
                     public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
-                        productList.clear(); // Clear previous data
+                        productList.clear();
                         for (QueryDocumentSnapshot document : queryDocumentSnapshots) {
                             Product product = document.toObject(Product.class);
                             productList.add(product);
                         }
-
-                        // Notify the adapter that the data has changed
                         adapter.notifyDataSetChanged();
                     }
                 })
@@ -190,7 +174,6 @@ public class MainActivity extends AppCompatActivity {
                     @Override
                     public void onFailure(@NonNull Exception e) {
                         Log.e("Firestore", "Error retrieving filtered data: " + e.getMessage());
-                        // Handle errors
                     }
                 });
     }
